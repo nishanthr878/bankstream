e3332fd526795229d6283e967835a8b119300e74
nishanthr878 <nishanthr878@gmail.com>
2026-05-23 20:27:54 +0530

    fix: using wrong JsonSerilazer due with

--- notification-service/src/main/java/com/bankstream/notification/config/KafkaProducerConfig.java
+++ notification-service/src/main/java/com/bankstream/notification/config/KafkaProducerConfig.java
@@ -1,6 +1,6 @@
 package com.bankstream.notification.config;
 
-import com.fasterxml.jackson.databind.JsonSerializer;
+import org.springframework.kafka.support.serializer.JsonSerializer;
 
 import org.apache.kafka.clients.producer.ProducerConfig;
 import org.apache.kafka.common.serialization.StringSerializer;
